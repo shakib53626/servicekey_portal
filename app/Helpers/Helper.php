0d7fc1ce999d4f59351e3fb508b5850e4646b7d3
shakib53626 <diu.shakib@gmail.com>
2024-02-16 16:05:37 +0600

    authentication done

---
+++ app/Helpers/Helper.php
@@ -0,0 +1,12 @@
+<?php
+
+
+function send_ms($msg,$status,$code){
+
+   return $res = [
+        'status' => $status,
+        'message' => $msg,
+   ];
+    return response()->json($res,$code);
+}
+
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        \App\Models\User::factory(10)->create();
+
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@gmail.com',
+            'phone' => '0172260010',
+            'password' => Hash::make('password'),
+        ]);
+
+        $this->call([
+            AdminsTableSeeder::class,
+            SellersTableSeeder::class,
+        ]);
+    }
+}
